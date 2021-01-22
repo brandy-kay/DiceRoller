6556e69d196c83c60fc7186997b8e3201d555b54
brandy-kay <brandyariana99@gmail.com>
2021-01-22 08:09:18 +0300

    simple implementation

--- app/src/main/java/adhanjas/com/example/diceroller/MainActivity.java
+++ app/src/main/java/adhanjas/com/example/diceroller/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
     TextView textView;
     Button button;
@@ -19,8 +21,52 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                textView=findViewById(R.id.txt);
+               rollDice();
             }
         });
+    }
+
+    private void rollDice() {
+        Dice dice =new Dice(6);
+        dice.roll();
+       textView=findViewById(R.id.txt);
+      // String text=textView.getText().toString();
+        // textView.setText(text);
+        switch (dice.roll()){
+            case 1:
+                textView.setText("1");
+                break;
+            case 2:
+                textView.setText("2");
+                break;
+            case 3:
+                textView.setText("3");
+                break;
+            case 4:
+                textView.setText("4");
+                break;
+            case 5:
+                textView.setText("5");
+                break;
+            case 6:
+                textView.setText("6");
+                break;
+            default:
+                textView.setText("empty dice");
+        }
+    }
+
+
+}
+
+class Dice{
+    public int number;
+    public Dice(int number){
+        this.number=number;
+    }
+    public int roll(){
+        Random random=new Random();
+        return random.nextInt(number);
+
     }
 }
